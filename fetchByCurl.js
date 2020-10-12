11ca234196f2cd328eb12410b7786aa4bfb981a3
Taisuke Fukuno <fukuno@jig.jp>
2020-10-12 23:34:09 +0900

    fix add close Process

--- fetchByCurl.js
+++ fetchByCurl.js
@@ -1,6 +1,7 @@
 const fetchByCurl = async (url) => {
   const p = Deno.run({ cmd: ["curl", url], stdout: "piped" });
   const html = new TextDecoder().decode(await p.output(), "utf-8");
+  p.close();
   return html;
 }
 
